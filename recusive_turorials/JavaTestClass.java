85e67f3310c798836875a365b447efcf5248121d
Kehinde-Crypto <kehindebrowns22@gmail.com>
2024-12-20 22:49:38 +0100

    Update JavaTestClass.java
    Use the scanner libary

--- recusive_turorials/JavaTestClass.java
+++ recusive_turorials/JavaTestClass.java
@@ -1,6 +1,7 @@
 import java.util.Scanner
 public class JavaTestClass {
   public static void main (String[]args){
+    Scanner scanner = new Scanner(System.in);
     int n = 10, firstNum = 0 , secondNum = 1;
     int nextNum;
 
